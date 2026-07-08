4bdbf4da794035168747938b1cbccc5c033e0361
esonoBibang <antonioesonobibang@g,ail.com>
2026-07-08 21:49:53 +0800

    campo_texto

---
+++ Mes_1/TEXFIELD_.py
@@ -0,0 +1,28 @@
+
+#TRABAJANDO CON CAMPO DE TEXTO
+import flet as ft
+from rich import color
+
+
+def main(page: ft.Page):
+    page.title = "Trabajando con TextField"
+
+    def mostrar_texto(e):
+        texto.value = campo_texto.value
+        campo_texto.value = ""
+
+    campo_texto = ft.TextField(
+        label="Escriba aquí",
+        hint_text="Su nombre",
+        on_submit=mostrar_texto
+         )
+    texto = ft.Text()
+
+
+
+    page.add(campo_texto, texto)
+
+
+
+
+ft.app(main)
